b439c1af70b2e90d29320e8fcb07b4b415c496ff
JUAN DAVID ZAMBRANO GUARIN <jd.zambrano@sis.virtual.uniandes.edu.co>
2018-02-11 13:58:20 -0500

    Creados los DTOs de calificacion y el Resource de Calificacion, falta implementarlos y documentarlos

---
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/CalificacionDTO.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sierra.dtos;
+
+/**
+ *
+ * @author jd.zambrano
+ */
+public class CalificacionDTO {
+    
+    private long ID;
+    
+    private String comentarios;
+    
+    private double valor;
+    
+    private String sugerencia;
+
+    /**
+     * @return the ID
+     */
+    public long getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the comentarios
+     */
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    /**
+     * @param comentarios the comentarios to set
+     */
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the sugerencia
+     */
+    public String getSugerencia() {
+        return sugerencia;
+    }
+
+    /**
+     * @param sugerencia the sugerencia to set
+     */
+    public void setSugerencia(String sugerencia) {
+        this.sugerencia = sugerencia;
+    }
+    
+}
