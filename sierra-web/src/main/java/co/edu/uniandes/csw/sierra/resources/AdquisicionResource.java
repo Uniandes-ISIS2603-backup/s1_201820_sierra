3d2785b13b593d8298dc1daa0c093456e62f20c0
JUAN DAVID ZAMBRANO GUARIN <jd.zambrano@sis.virtual.uniandes.edu.co>
2018-02-11 13:11:57 -0500

    implementados los metodos vacios de AdquisicionResource, falta la documentacion de dicha clase

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/AdquisicionResource.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/AdquisicionResource.java
@@ -6,10 +6,18 @@
 package co.edu.uniandes.csw.sierra.resources;
 
 import co.edu.uniandes.csw.sierra.dtos.AdquisicionDetailDTO;
+import co.edu.uniandes.csw.sierra.dtos.SierraDetailDTO;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -31,5 +39,31 @@ public class AdquisicionResource {
     @POST
     public AdquisicionDetailDTO createAdquisicion(AdquisicionDetailDTO dto){
         return dto;
+    }
+    
+    @GET
+    public ArrayList<AdquisicionDetailDTO> getAdquisiciones(){
+        return new ArrayList<>();
+    }
+    
+    @GET
+    @Path( "{id: \\d+}" )
+    public AdquisicionDetailDTO getAdquisicion( @PathParam( "id" ) Long id )
+    {
+        return null;
+    }
+    
+    @PUT
+    @Path( "{id: \\d+}" )
+    public AdquisicionDetailDTO updateAdquisicion( @PathParam( "id" ) Long id, AdquisicionDetailDTO dDTO ) throws BusinessLogicException
+    {
+	return dDTO;
+    }
+    
+    @DELETE
+    @Path( "{id: \\d+}" )
+    public void deleteAdquisicion( @PathParam( "id" ) Long id )
+    {
+    	
     }
 }
