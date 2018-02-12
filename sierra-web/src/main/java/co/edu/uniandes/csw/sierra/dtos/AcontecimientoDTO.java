e21353bae813e33288a8ee5656399c72fc5d298d
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-12 08:36:04 -0500

    agregados los DTOs de Acontecimiento y Publicacion, modificados los Adquisicion y Calificacion para que los Longs sean objetos y no tipos primitivos

---
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/AcontecimientoDTO.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sierra.dtos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jd.zambrano
+ */
+public class AcontecimientoDTO {
+    
+    private Long id;
+    
+    private String nombre;
+    
+    private String descripcion;
+    
+    private Date fecha;
+    
+    private Integer importancia;
+    
+    private String tipo;
+    
+    private String fotoURL;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the importancia
+     */
+    public int getImportancia() {
+        return importancia;
+    }
+
+    /**
+     * @param importancia the importancia to set
+     */
+    public void setImportancia(int importancia) {
+        this.importancia = importancia;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the fotoURL
+     */
+    public String getFotoURL() {
+        return fotoURL;
+    }
+
+    /**
+     * @param fotoURL the fotoURL to set
+     */
+    public void setFotoURL(String fotoURL) {
+        this.fotoURL = fotoURL;
+    }
+    
+    
+}
---
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/PublicacionDTO.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sierra.dtos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jd.zambrano
+ */
+public class PublicacionDTO {
+    
+    private Long id;
+    
+    private String comentario;
+    
+    private String fotoURL;
+    
+    private String tipo;
+    
+    private Date fecha;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the comentario
+     */
+    public String getComentario() {
+        return comentario;
+    }
+
+    /**
+     * @param comentario the comentario to set
+     */
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    /**
+     * @return the fotoURL
+     */
+    public String getFotoURL() {
+        return fotoURL;
+    }
+
+    /**
+     * @param fotoURL the fotoURL to set
+     */
+    public void setFotoURL(String fotoURL) {
+        this.fotoURL = fotoURL;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+}
