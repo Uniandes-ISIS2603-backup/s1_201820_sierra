48f23764e2dac42e83e03448ab3b910c9711f069
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-12 21:33:02 -0500

    se agregaron las licencias a los DTOs Calificacion y Adquisicion, se terminó la documentación de los DTOs mencionados, falta una ultima revision a esa documentacion

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/AdquisicionDetailDTO.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/AdquisicionDetailDTO.java
@@ -1,19 +1,153 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+MIT License
+Copyright (c) 2017 ISIS2603
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 package co.edu.uniandes.csw.sierra.dtos;
 
 /**
- *
- * @author jd.zambrano
+ * AdquisicionDTO es el objeto de transferencia de datos detallada de la entidad Adquisicion.
+ * <p>
+ * Al serializarse como JSON, esta clase implementa el siguiente modelo:<br>
+ * <pre>
+ *  {
+ *      "id":number,
+ *      "valorTotal":number,
+ *      "fecha":date,
+ *      "calificacion":{
+ *          "id":number,
+ *          "comentarios":string,
+ *          "valor":number,
+ *          "sugerencia":string
+ *      },
+ *      "mascota":{
+ *          "id": number,
+ *          "name: String,
+ *          "genero": String
+ *          "edad": number,
+ *          "color": String,
+ *          "esteril":boolean,
+ *          "adquirido":boolean,
+ *          "imagen":String,
+ *          "tamano":String,
+ *          "nacimiento": Date,
+ *          "muerte": Date
+ *      },
+ *      "factura":"FALTA EL DTO DE FACTURA!!!!!",
+ *      "cliente":{
+ *          "name": string,
+ *          "apellido": string,
+ *          "cedula": number,
+ *          "id": number,
+ *          "telefono": number
+ *      }
+ *  }
+ * </pre>
+ * * Por ejemplo, una Adquisicion se representaria de la siguiente manera:<br>
+ * <p>
+ * <pre>
+ *  {
+ *      "id":1,
+ *      "valorTotal":10000,
+ *      "fecha":"20/05/2014",
+ *      "calificacion":{
+ *          "id":5,
+ *          "comentarios":"muy buenna pagina, todo funciona super bien y fue muy buena la experiencia",
+ *          "valor":20000,
+ *          "sugerencia":"You don't mess with perfection"
+ *      },
+ *      "mascota":{
+ *          "id": 31,
+ *          "name: "Ircops",
+ *          "edad": 2,
+ *          "color":"negro",
+ *          "esteril":true,
+ *          "adquirido":false,
+ *          "imagen":"https://c1.staticflickr.com/5/4174/33634058024_74d000546d_b.jpg"
+ *          "tamano":"Grande",
+    *      "nacimiento": 03/10/2016,
+ *          "muerte": 
+ *      },
+ *      "factura":"FALTA EL DTO DE FACTURA!!!!!(si, otra vez)",
+ *      "cliente":{
+ *          "name": "Andres",
+ *          "apellido": "Castro",
+ *          "cedula": 1072548232,
+ *          "id": 001,
+ *          "telefono": 3182564852
+ *      }
+ *  }
+ * </pre>
+ * @author Juan David Zambrano
  */
 public class AdquisicionDetailDTO extends AdquisicionDTO{
     
+    private CalificacionDTO calificacion;
     
+    private MascotaDTO mascota;
+    
+    //TODO: hacer toca que alguien suba facturaDTO
+    //private FacturaDTO factura
+    
+    private ClienteDTO cliente;
     
     public AdquisicionDetailDTO(){
         super();
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public CalificacionDTO getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(CalificacionDTO calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the mascota
+     */
+    public MascotaDTO getMascota() {
+        return mascota;
+    }
+
+    /**
+     * @param mascota the mascota to set
+     */
+    public void setMascota(MascotaDTO mascota) {
+        this.mascota = mascota;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public ClienteDTO getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(ClienteDTO cliente) {
+        this.cliente = cliente;
     }
 }
--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/CalificacionDetailDTO.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/CalificacionDetailDTO.java
@@ -1,18 +1,79 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+MIT License
+Copyright (c) 2017 ISIS2603
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 package co.edu.uniandes.csw.sierra.dtos;
 
 /**
- *
- * @author jd.zambrano
+ * CalificacionDetailDTO es el objeto de transferencia de datos detallada de la entidad Calificacion.
+ * <p>
+ * Al serializarse como JSON, esta clase implementa el siguiente modelo:<br>
+ * <pre>
+ *  {
+ *      "id":number,
+ *      "comentarios":string,
+ *      "valor":number,
+ *      "sugerencia":string,
+ *      "adquisicion":
+ *          {
+ *          "id":number,
+ *          "valorTotal":number,
+ *          "fecha":date
+ *          }
+ *  }
+ * </pre>
+ * * Por ejemplo, una Calificacion detallada se representaria de la siguiente manera:<br>
+ * <p>
+ * <pre>
+ *  {
+ *      "id":5,
+ *      "comentarios":"muy buenna pagina, todo funciona super bien y fue muy buena la experiencia",
+ *      "valor":20000,
+ *      "sugerencia":"You don't mess with perfection",
+ *      "adquisicion":
+ *          {
+ *          "id":3,
+ *          "valorTotal":10000000,
+ *          "fecha":"10/5/1973"
+ *          }
+ *  }
+ * </pre>
+ * @author Juan David Zambrano
  */
 public class CalificacionDetailDTO extends CalificacionDTO{
     
+    private AdquisicionDTO adquisicion;
     
     public CalificacionDetailDTO(){
         super();
+    }
+
+    /**
+     * @return the adquisicion
+     */
+    public AdquisicionDTO getAdquisicion() {
+        return adquisicion;
+    }
+
+    /**
+     * @param adquisicion the adquisicion to set
+     */
+    public void setAdquisicion(AdquisicionDTO adquisicion) {
+        this.adquisicion = adquisicion;
     }
 }
