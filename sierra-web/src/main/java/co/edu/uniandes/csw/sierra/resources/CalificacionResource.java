82e08571466eb9085a98173f6a00a25a0437a127
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-13 07:39:15 -0500

    Se documentan los GET y POST de CalificacionResource

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/CalificacionResource.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/CalificacionResource.java
@@ -20,25 +20,83 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
- *
- * @author Juan David Zambrano
- */
+* <pre>Clase que implementa el recurso "Calificaciones". URL: /api/calificaciones
+* </pre>
+* <i>Note que la aplicación (definida en {@link RestConfig}) define la ruta "/api"
+* y este recurso tiene la ruta "calificaciones".</i>
+* <h2> Anotaciones </h2>
+* <pre>
+* Path: indica la dirección después de "api" para acceder al recurso
+* Produces/Consumes: indica que los servicios definidos en este recurso reciben y
+devuelven objetos en formato JSON
+* RequestScoped: Inicia una transacción desde el llamado de cada método (servicio).
+* </pre>
+* @author Juan David Zambrano
+* @version 1.0
+*/
 @Path( "calificaciones" )
 @Produces( "application/json" )
 @Consumes( "application/json" )
 @RequestScoped
 public class CalificacionResource {
     
-    
+    /**
+     * <h1> POST /api/calificaciones : Crea una calificacion. </h1>
+     * <pre> Cuerpo de la peticion: JSON (@link CalificacionDetailDTO).
+     * Crea una nueva calificacion con la informacion que entra por el cuerpo de la peticion.
+     * Retorna un nuevo objeto identico con un id autogenerado por la base de datos
+     * Codigos de respuesta:
+     * <code style="color:mediumseagreen; backround-color: #eaffe0;">
+     * 200 OK La calificacion fue creada
+     * </code>
+     * <code style=" color: #c7254e; backround-color: #f9f2f4;">
+     * 412 Precondition Failed: Ya existe una calificacion asociada a esa Adquisicion
+     * </code>
+     * </pre>
+     * @param dto (@link CalificacionDetailDTO) - La nueva calificacion
+     * @return JSON (@link CalificacionDetailDTO) - La calificacion guardada con el atributo id
+     * @throws BusinessLogicException Error de logica: ya existe una calificacion asociada a la adquisicion dada
+     */
     @POST
-    public CalificacionDetailDTO createCalificacion(CalificacionDetailDTO dto){
+    public CalificacionDetailDTO createCalificacion(CalificacionDetailDTO dto) throws BusinessLogicException{
         return dto;
     }
     
+    /**
+    * <h1> GET /api/calificaciones : Obtener todas las calificaciones.</h1>
+    *
+    * <pre> Busca y devuelve todas las calificaciones que existen en la
+    * aplicación.
+    *
+    * Códigos de respuesta:
+    * <code style="color: mediumseagreen; background-color: #eaffe0;">
+    * 200 OK Devuelve todas las calificaciones de la aplicación
+    * </code>
+    * </pre>
+    * @return JSONArray {@link CalificacionDetailDTO} - Las calificaciones
+    * encontradas en la aplicación. Si no hay ninguna retorna una
+    * lista vacía.
+    */
     @GET
     public List<CalificacionDetailDTO> getCalificaciones(){
         return new ArrayList<>();
     }
+    
+    /**
+     * <h1> GET /api/calificaciones/{id}: Obtener la calificacion por id.</h1>
+     * <pre> Busca la calificacion con el id recibido en el URL y lo retorna
+     * 
+     * Codigos de respuesta:
+     * <code style = "color: mediumseagreen; backround-color: #eaffe0;">
+     * 200 OK Devuelve la calificacion asociada al id.
+     * </code>
+     * <code style = "color: #c7254e; backround-color: #f9f2f4;">
+     * 404 Not Found: No existe la calificacion con el id dado
+     * </code>
+     * </pre>
+     * @param id el identificador unico de la calificacion, debe ser una cadena de digitos
+     * @return JSON (@link CalificacionDetailDTO) - La calificacion buscada
+     */
     
     @GET
     @Path( "{id: \\d+}" )
