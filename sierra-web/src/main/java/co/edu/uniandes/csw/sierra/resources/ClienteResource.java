405a33288659f696fc63309d8e61e5ecf0576722
Estebangp <de.gutierrez@uniandes.edu.co>
2018-02-12 19:18:02 -0500

    Clases resource de cliente y medio de pago.

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/ClienteResource.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/ClienteResource.java
@@ -13,7 +13,18 @@ import javax.ws.rs.*;
 import javax.ws.rs.Produces;
 
 /**
- *
+ *<pre>Clase que implementa el recurso "cliente".
+ * URL: /api/clientes
+ * </pre>
+ * <i>Note que la aplicación (definida en {@link RestConfig}) define la ruta "/api" y
+ * este recurso tiene la ruta "clientes".</i>
+ * <p>
+ * <h2>Anotaciones </h2>
+ * <pre>
+ * Path: indica la dirección después de "api" para acceder al recurso
+ * Produces/Consumes: indica que los servicios definidos en este recurso reciben y devuelven objetos en formato JSON
+ * RequestScoped: Inicia una transacción desde el llamado de cada método (servicio).
+ * </pre>
  * @author de.gutierrez
  */
 @Path("clientes")
@@ -23,11 +34,13 @@ import javax.ws.rs.Produces;
 public class ClienteResource 
 {
     /**
-     * <h1> POST /api/clientes: Crea un nuevo cliente</h1>
+     * <h1> POST /api/clientes : Crea un nuevo cliente</h1>
      * <p>
      * <pre> Cuerpo de peticion: JSON {@link ClienteDetailDTO}.
      * 
-     * Crea un nuevo cliente con la informacion que se recibe...
+     * Crea un nuevo cliente con la informacion que se recibe en el cuerpo 
+     * de la peticion y se regresa un objeto identico con un id auto-generado
+     * por la base de datos.
      * 
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
@@ -38,17 +51,18 @@ public class ClienteResource
      * </code>
      * </pre>
      * @param cliente {@link ClienteDetailDTO} - El cliente que se desea guardar
-     * @return JSON {@link ClienteDetailDTO} - El cliente guardado con el atributo id gererado
+     * @return JSON {@link ClienteDetailDTO} - El cliente guardado con el atributo id generado
      * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de logica que se genera cuando ya existe un cliente.
      */
     @POST
-    public ClienteDTO createCliente(ClienteDTO cliente) throws BusinessLogicException
+    public ClienteDetailDTO createCliente(ClienteDetailDTO cliente) throws BusinessLogicException
     {
         return cliente;
     }
    
     /**
      * <h1>GET /api/clientes : Obtener todos los clientes.</h1>
+     * <p>
      * <pre> Busca y devuelve todos los clientes.
      * Codigos de respuesta:
      * <code style="color: mediumseagreen; background-color: #eaffe0;">
@@ -84,5 +98,50 @@ public class ClienteResource
     public ClienteDetailDTO getCliente(@PathParam("id") Long id)
     {
         return null;
+    }
+    
+    /**
+     * <h1>  PUT /api/clientes/{id} : Actualizar una entidad de cliente con el id dado. </h1>
+     * <pre> Cuerpo de peticion: JSON {@link ClienteDetailDTO}.
+     * 
+     * Actualiza la entidad de cliente con el id recibido en la URL con la informacion que se recibe en el cuerpo de la peticion.
+     * 
+     ** Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Actualiza la entidad de Cliente con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una entidad de Cliente con el id dado.
+     * </code>
+     * </pre>
+     * @param id Identificador de la entidad de cliente que se desea actualizar. Este debe ser una cadena de digitos.
+     * @param infoCliente {@link ClienteDetailDTO} la entidad de cliente que se desea guardar.
+     * @return JSON {@link ClienteDetailDTO} - La entidad de cliente guardada.
+     * @throws BusinessLogicException {@link BusinessLogicException}  Error de logica que se genera al no poder actualizar la entidad de Cliente porque ya existe una con ese nombre.
+     */
+    @PUT 
+    @Path( "{id: \\d+}")
+    public ClienteDetailDTO updateCliente (@PathParam ("id") Long id, ClienteDetailDTO infoCliente) throws BusinessLogicException
+    {
+        return infoCliente;
+    }
+    
+    /**
+     * <h1> DELETE /api/clientes/{id} : Borra un cliente con el id. </h1>
+     * <p>
+     * <pre> Borra la entidad cliente con el id asociado recibido en la URL.
+     * Códigos de respuesta:<br>
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Elimina la entidad de Cliente correspondiente al id dado.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una entidad de Cliente con el id dado. </code>
+     * </pre>
+     * 
+     * @param id Identificar de la entidad del cliente que se desea borrar. Este debe ser una cadena de digitos.
+     */
+    @DELETE
+    @Path ( "{id: \\d+}" )
+    public void deleteCliente (@PathParam ("id") Long id)
+    {
+        //voi
     }
 }
---
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/MetodoDePagoResource.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sierra.resources;
+
+import co.edu.uniandes.csw.sierra.dtos.MedioDePagoDetail;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import java.util.*;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.*;
+
+/**
+ *<pre>Clase que implementa el recurso "medioDePago".
+ * URL: /api/mediosDePago
+ * </pre>
+ * <i>Note que la aplicación (definida en {@link RestConfig}) define la ruta "/api" y
+ * este recurso tiene la ruta "mediosDePago".</i>
+ * <p>
+ * <h2>Anotaciones </h2>
+ * <pre>
+ * Path: indica la dirección después de "api" para acceder al recurso
+ * Produces/Consumes: indica que los servicios definidos en este recurso reciben y devuelven objetos en formato JSON
+ * RequestScoped: Inicia una transacción desde el llamado de cada método (servicio).
+ * </pre>
+ * @author de.gutierrez
+ */
+@Path( "mediosDePago")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class MetodoDePagoResource 
+{
+    /**
+     * <h1> POST /api/mediosDePago : Crea un nuevo medio de pago.</h1>
+     * <p>
+     * <pre> Cuerpo de la peticion: JSON {@link MedioDePagoDetail}.
+     * 
+     * Crea un nuevo medio de pago con la informacion que se recibe en el cuerpo 
+     * de la peticion y se regresa un objeto identico con un id auto-generado
+     * por la base de datos.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 PK Creo un nuevo medio de pago.
+     * </code>
+     * <code style="color: #c7254; background-color: #f9f2f4;">
+     * 412 Precondition Failed: ya existe ese medio de pago:
+     * </code>
+     * </pre>
+     * 
+     * @param medio {@link MedioDePagoDetail} - El medio de pago que se desea guardar.
+     * @return JSON {@link MedioDePagoDetail}- El medio de pago guardado con el  atributo id autogenerado.
+     * @throws BusinessLogicException {@link BusinessLogicException} -  Error de logica que se genera cuando ya existe un medio de pago.
+     */
+    @POST
+    public MedioDePagoDetail createMedioDePago(MedioDePagoDetail medio) throws BusinessLogicException
+    {
+        return medio;
+    }
+    
+    /**
+     * <h1> GET /api/mediosDePago : Obtener todos los medios de pago que acepta el negocio. </h1>
+     * <p>
+     * <pre> Busca y devuelve todos los medios de pago.
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve todos los medioDePago de la aplicacion.</code>
+     * </pre>
+     * @return JSONArray {@link  MedioDePagoDetail} - Los medios de pago en la aplicacion. Si no hay ninguno retorna vacio. 
+     */
+    @GET
+    public List<MedioDePagoDetail> getMediosDePago() 
+    {
+        return new ArrayList<>();
+    }
+    
+    /**
+     * <h1> GET /api/mediosDePago/{id} Obtener un medio de pago con el id.</h1>
+     * <p>
+     * <pre> Busca un medio de pago con el id asociado recibido por la URL y la devuelve.
+     * 
+     * Codigos de respuesta:
+     * 
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve el medioDePago correspondiente al id.
+     * </code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found No existe un medioDePago con el id dado.
+     * </code>
+     *</pre>
+     * @param id Identificador del medio de pago que se esta buscando. Este debe ser una cadena de digitos.
+     * @return JSON {@link MedioDePagoDetail} - El medio de pago buscado.
+     * 
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public MedioDePagoDetail getMedioDePago(@PathParam ("id") Long id)  
+    {
+        return null;
+    }
+    
+    /**
+     * <h1> PUT /api/mediosDePago/{id} : Actualizar un medio de pago. </h1>
+     * <pre> Cuerpo de peticion: JSON {@link MedioDePagoDetail}.
+     * 
+     * Actualiza el medio de pago con el id recibido en la URL con la informacion que se recibe en el cuerpo de la peticion.
+     ** Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Actualiza la entidad de medioDePago con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una entidad de medioDePago con el id dado.
+     * </code>
+     * </pre>
+     * @param id Identificador de la entidad medio de pago que se desea actualizar. Este debe ser una cadena de digitos.
+     * @param medio {@link MedioDePagoDetail} La entidad de medio de pago que se desea guardar.
+     * @return JSON {@link MedioDePagoDetail} - La entidad de medio de pago guardada.
+     *  @throws BusinessLogicException {@link BusinessLogicException}  Error de logica que se genera al no poder actualizar la entidad de Cliente porque ya existe una con ese nombre.
+     */
+    @PUT
+    @Path("{id: \\d++}")
+    public MedioDePagoDetail updateMedioDePago(@PathParam("id") Long id, MedioDePagoDetail medio) throws BusinessLogicException
+    {
+        return medio;
+    }
+    
+    /**
+     * <h1> DELETE /api/mediosDePago7{id} : Borra un medio de pago.</h1>
+     * <p>
+     * <pre> Borra la entidad medioDePago con el id asociado recibido en la URL.
+     * Códigos de respuesta:<br>
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Elimina la entidad de medioDePago correspondiente al id dado.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una entidad de medioDePago con el id dado. </code>
+     * </pre>
+     *
+     * @param id Identificador de la entidad de medioDePago que se desea borrar. Este debe ser una cadena de digitos.
+     */
+    @DELETE
+    @Path ("{id: \\d++}")
+    public void deleteMedioDePago ( @PathParam ("id") Long id)
+    {
+        
+    }
+}
