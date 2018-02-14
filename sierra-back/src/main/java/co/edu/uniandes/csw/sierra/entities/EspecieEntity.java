ed8d4307e7c297c4c9c812a8a5968b2111e67966
Juan Sanchez <jc.sanchez12@uniandes.edu.co>
2018-02-14 11:31:48 -0500

    Mascota y Especie entity implementadas sin listas

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/EspecieEntity.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/EspecieEntity.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.csw.sierra.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
@@ -13,5 +14,102 @@ import java.io.Serializable;
  */
 public class EspecieEntity extends BaseEntity implements Serializable
 {
+    //Atributos
+      /**
+     * nombre de la especie
+     */
+    private String nombre;
+    /**
+     * caracteristicas de la  especie
+     */
+    private String caracteristicas;
+    
+    /**
+     * Clasificacion de la  especie
+     */
+    private String clasificacion;
+    
+    
+    /**
+     * Lista de mascotas que contiene una  especie
+     */
+    private List<MascotaEntity> mascotas;
+ 
+    /**
+     * Lista de razas que contiene una  especie
+     */
+    private List<RazaEntity> razas;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the caracteristicas
+     */
+    public String getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    /**
+     * @param caracteristicas the caracteristicas to set
+     */
+    public void setCaracteristicas(String caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    /**
+     * @return the clasificacion
+     */
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    /**
+     * @param clasificacion the clasificacion to set
+     */
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    /**
+     * @return the mascotas
+     */
+    public List<MascotaEntity> getMascotas() {
+        return mascotas;
+    }
+
+    /**
+     * @param mascotas the mascotas to set
+     */
+    public void setMascotas(List<MascotaEntity> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    /**
+     * @return the razas
+     */
+    public List<RazaEntity> getRazas() {
+        return razas;
+    }
+
+    /**
+     * @param razas the razas to set
+     */
+    public void setRazas(List<RazaEntity> razas) {
+        this.razas = razas;
+    }
+    
+    
     
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/MascotaEntity.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/MascotaEntity.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.sierra.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -66,5 +67,222 @@ public class MascotaEntity extends BaseEntity implements Serializable
      */
     private Date muerte;
     
+     /**
+     * Especie de la mascota
+     */
+    private EspecieEntity especie;
     
+    /**
+     * Raza de la  mascota
+     */
+    private RazaEntity raza;
+    
+    /**
+     * Publicaciones de la mascota
+     */
+     private List<PublicacionEntity> publicaciones;
+     
+     /**
+      *  Adquisiciones de a mascota
+      */
+      private List<AdquisicionEntity> adquisiciones;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the esteril
+     */
+    public boolean isEsteril() {
+        return esteril;
+    }
+
+    /**
+     * @param esteril the esteril to set
+     */
+    public void setEsteril(boolean esteril) {
+        this.esteril = esteril;
+    }
+
+    /**
+     * @return the adquirida
+     */
+    public boolean isAdquirida() {
+        return adquirida;
+    }
+
+    /**
+     * @param adquirida the adquirida to set
+     */
+    public void setAdquirida(boolean adquirida) {
+        this.adquirida = adquirida;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    /**
+     * @return the tamano
+     */
+    public String getTamano() {
+        return tamano;
+    }
+
+    /**
+     * @param tamano the tamano to set
+     */
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    /**
+     * @return the nacimiento
+     */
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    /**
+     * @param nacimiento the nacimiento to set
+     */
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    /**
+     * @return the muerte
+     */
+    public Date getMuerte() {
+        return muerte;
+    }
+
+    /**
+     * @param muerte the muerte to set
+     */
+    public void setMuerte(Date muerte) {
+        this.muerte = muerte;
+    }
+
+    /**
+     * @return the especie
+     */
+    public EspecieEntity getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(EspecieEntity especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return the raza
+     */
+    public RazaEntity getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(RazaEntity raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the publicaciones
+     */
+    public List<PublicacionEntity> getPublicaciones() {
+        return publicaciones;
+    }
+
+    /**
+     * @param publicaciones the publicaciones to set
+     */
+    public void setPublicaciones(List<PublicacionEntity> publicaciones) {
+        this.publicaciones = publicaciones;
+    }
+
+    /**
+     * @return the adquisiciones
+     */
+    public List<AdquisicionEntity> getAdquisiciones() {
+        return adquisiciones;
+    }
+
+    /**
+     * @param adquisiciones the adquisiciones to set
+     */
+    public void setAdquisiciones(List<AdquisicionEntity> adquisiciones) {
+        this.adquisiciones = adquisiciones;
+    }
+      
+      
+      
 }
