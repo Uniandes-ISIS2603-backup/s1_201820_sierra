29d5bbda6993973e4be5ecc88c6a331006dae217
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-14 13:39:36 -0500

    se implementan las entidades Calificacion y Adquisicion. Estas no tienen asociaciones aún

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/AdquisicionEntity.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/AdquisicionEntity.java
@@ -5,10 +5,51 @@
  */
 package co.edu.uniandes.csw.sierra.entities;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+
 /**
- *
- * @author jc.sanchez12
+ * Entidad que representa una Adquisicion.
+ * 
+ * 
+ * @author Juan David Zambrano
  */
-public class AdquisicionEntity {
+@Entity
+public class AdquisicionEntity extends BaseEntity implements Serializable{
+    
+    private Double valorTotal;
+       
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha;
+
+    /**
+     * @return the valorTotal
+     */
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
     
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/CalificacionEntity.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/CalificacionEntity.java
@@ -5,10 +5,65 @@
  */
 package co.edu.uniandes.csw.sierra.entities;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+
 /**
- *
- * @author jc.sanchez12
+ * Entidad que representa una calificacion.
+ * 
+ * @author Juan David Zambrano
  */
-public class CalificacionEntity {
+@Entity
+public class CalificacionEntity extends BaseEntity implements Serializable{
+    
+    
+    private String comentarios;
+    
+    private Double valor;
+    
+    private String sugerencia;
+
+    /**
+     * @return the comentarios
+     */
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    /**
+     * @param comentarios the comentarios to set
+     */
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    /**
+     * @return the valor
+     */
+    public Double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the sugerencia
+     */
+    public String getSugerencia() {
+        return sugerencia;
+    }
+
+    /**
+     * @param sugerencia the sugerencia to set
+     */
+    public void setSugerencia(String sugerencia) {
+        this.sugerencia = sugerencia;
+    }
+
     
 }
