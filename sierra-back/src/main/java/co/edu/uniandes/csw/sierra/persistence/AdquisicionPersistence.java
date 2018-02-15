055ed94db33de968d991ae11a9ccff9d4bb654ba
Juan Sanchez <jc.sanchez12@uniandes.edu.co>
2018-02-15 07:46:23 -0500

    Se agregaron todos los persistence

---
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/AdquisicionPersistence.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.sierra.persistence;
+
+import co.edu.uniandes.csw.sierra.ejb.*;
+
+/**
+ *
+ * @author jc.sanchez12
+ */
+public class AdquisicionPersistence {
+    
+}
