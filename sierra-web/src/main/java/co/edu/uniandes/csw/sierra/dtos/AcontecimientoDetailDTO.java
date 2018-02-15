0a944416fb6ba2b4dd8d9afb83d2f285dcf20099
Granja <>
2018-02-14 19:47:46 -0500

    Acontecimiento Resource

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/AcontecimientoDetailDTO.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/AcontecimientoDetailDTO.java
@@ -6,13 +6,58 @@
 package co.edu.uniandes.csw.sierra.dtos;
 
 /**
- *
- * @author jd.zambrano
- */
+ * AcontecimientoDTO es el objeto de transferencia de datos detallada de la entidad Acontecimiento.
+ * <p>
+ * Al serializarse como JSON, esta clase implementa el siguiente modelo:<br>
+ * <pre>
+ * {
+ * "id":number,
+ * "nombre": String,
+ * "descripcion":String,
+ * "fecha":Date,
+ * "importancia":number,
+ * "tipo": String,
+ * "fotoURL":String,
+ },
+ * "mascota":{
+ *          "id": number,
+ *          "name: String,
+ *          "genero": String
+ *          "edad": number,
+ *          "color": String,
+ *          "esteril":boolean,
+ *          "adquirido":boolean,
+ *          "imagen":String,
+ *          "tamano":String,
+ *          "nacimiento": Date,
+ *          "muerte": Date
+ *      }
+ * }
+ *  </pre>
+ * @author Ja.penat
+ **/
 public class AcontecimientoDetailDTO extends AcontecimientoDTO {
     
+    private MascotaDTO mascota;
     
-    public AcontecimientoDetailDTO(){
+    public AcontecimientoDetailDTO()
+    {
         super();
+ 
+    }
+     /**
+     * @return the mascota
+     */
+    public MascotaDTO getMascota() 
+    {
+        return mascota;
+    }
+
+    /**
+     * @param mascota the mascota to set
+     */
+    public void setMascota(MascotaDTO mascota)
+    {
+        this.mascota = mascota;
     }
 }
