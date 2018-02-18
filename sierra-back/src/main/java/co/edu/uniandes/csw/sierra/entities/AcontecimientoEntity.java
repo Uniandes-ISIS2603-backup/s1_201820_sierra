2fbc875fca8516bc69705b8b69c8468e5132db2c
Juan Sanchez <jc.sanchez12@uniandes.edu.co>
2018-02-17 19:46:35 -0500

    Test

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/AcontecimientoEntity.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/entities/AcontecimientoEntity.java
@@ -15,6 +15,8 @@ import javax.persistence.Temporal;
 @Entity
 public class AcontecimientoEntity extends BaseEntity implements Serializable
 {
+
+    private static final long serialVersionUID = 1L;
   
     private String nombre;
     
