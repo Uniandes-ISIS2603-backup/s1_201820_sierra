38c2b874381a255958d8daa29433e263677c4e2b
Granja <>
2018-02-18 15:51:51 -0500

    Clases de persistencia de acontecimiento y publicacion

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/AcontecimientoPersistence.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/AcontecimientoPersistence.java
@@ -4,13 +4,85 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.sierra.persistence;
-
-import co.edu.uniandes.csw.sierra.ejb.*;
+import co.edu.uniandes.csw.sierra.entities.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
- *
- * @author jc.sanchez12
+ *Clase encargada de la persistencia de acontecimiento
+ * @author ja.penat 
  */
-public class AcontecimientoPersistence {
+@Stateless 
+public class AcontecimientoPersistence 
+{
+    private static final Logger LOGGER = Logger.getLogger(AcontecimientoPersistence.class.getName());
+
+   // Contexto de la persistencia.
+    @PersistenceContext(unitName = "SierraPU")
+   //Entity manager.
+    protected EntityManager em;
     
+
+    /**
+     * Metodo encargado de crear un acontecimiento, que entra por parametro, en la base de datos de la aplicacion.
+     * @param acEntity la entidad que se quiere guardar en la base de datos
+     * @return la entity creada con el id dado por la base de datos
+     */
+    public AcontecimientoEntity create(AcontecimientoEntity  acEntity)
+    {
+        LOGGER.info("Creando una nueva entidad de Acontecimiento");
+        em.persist(acEntity);
+        LOGGER.info("Creando una entidad de Acontecimiento");
+        return acEntity;
+    }
+
+    /**
+     * Metodo encargado de retornar una lista con todos los acontecimientos en la base de datos de la aplicacion.
+     * @return la lista con todos los acontecimientos en la base de datos.
+     */
+    public List<AcontecimientoEntity> findAll()
+    {
+        LOGGER.info("Consultando todas las entidades de Acontecimiento");
+        TypedQuery<AcontecimientoEntity> query = em.createQuery("select u from AcontecimientoEntity u", AcontecimientoEntity.class);
+        return query.getResultList();
+    }
+
+    /**
+     * Metodo encargado de encontrar el  acontecimiento en la base de datos con el id dado por parametro.
+     * @param id del acontecimiento que se quiere buscar en la base de datos.
+     * @return El acontecimiento con el id suministrado.
+     */
+    public AcontecimientoEntity find(Long id)
+    {
+        LOGGER.log(Level.INFO, "Consultando el Acontecimiento con  el id={0}", id);
+        return em.find(AcontecimientoEntity.class, id);
+    }
+
+    /**
+     * Metodo encargado de actualizar el  acontecimiento en la base de datos.
+     * @param acEntity el acontecimiento que se quiere actualizar en la base de datos.
+     * @return El acontecimiento actualizado.
+     */
+    public AcontecimientoEntity update(AcontecimientoEntity acEntity) 
+    {
+        LOGGER.log(Level.INFO, "Actualizando el Acontecimiento con id={0}", acEntity.getId());
+        return em.merge(acEntity);
+    }
+
+    /**
+     * Metodo encargado de borrar el  acontecimiento en la base de datos.
+     * @param acEntity el acontecimiento que se quiere borrar en la base de datos.
+     */
+
+    public void delete(AcontecimientoEntity acEntity) 
+    {
+        LOGGER.log(Level.INFO, "Borrando el Acontecimiento con id={0}", acEntity.getId());
+        em.remove(acEntity);
+    }
+
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/PublicacionPersistence.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/PublicacionPersistence.java
@@ -1,16 +1,88 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.sierra.persistence;
 
-import co.edu.uniandes.csw.sierra.ejb.*;
+
+import co.edu.uniandes.csw.sierra.entities.PublicacionEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
- *
- * @author jc.sanchez12
+ *Clase encargada del la persistencia de publicacion.
+ * @author ja.penat
  */
-public class PublicacionPersistence {
+public class PublicacionPersistence 
+{
+      private static final Logger LOGGER = Logger.getLogger(PublicacionPersistence.class.getName());
+
+   // Contexto de la persistencia.
+    @PersistenceContext(unitName = "SierraPU")
+   //Entity manager.
+    protected EntityManager em;
     
+
+    /**
+     * Metodo encargado de crear una publicacion, que entra por parametro, en la base de datos de la aplicacion.
+     * @param pubEntity la entidad que se quiere guardar en la base de datos.
+     * @return la entity creada con el id dado por la base de datos
+     */
+    public PublicacionEntity create(PublicacionEntity  pubEntity)
+    {
+        LOGGER.info("Creando una nueva entidad de Publicacion");
+        em.persist(pubEntity);
+        LOGGER.info("Creando una entidad de Publicacion");
+        return pubEntity;
+    }
+
+    /**
+     * Metodo encargado de retornar una lista con todas las publicaciones en la base de datos de la aplicacion.
+     * @return la lista con todas las publicaciones en la base de datos.
+     */
+    public List<PublicacionEntity> findAll()
+    {
+        LOGGER.info("Consultando todas las entidades de Publicacion");
+        TypedQuery<PublicacionEntity> query = em.createQuery("select u from PublicacionEntity u", PublicacionEntity.class);
+        return query.getResultList();
+    }
+
+    /**
+     * Metodo encargado de encontrar la publicacion en la base de datos con el id dado por parametro.
+     * @param id de la publicacion que se quiere buscar en la base de datos.
+     * @return La publicacion con el id suministrado.
+     */
+    public PublicacionEntity find(Long id)
+    {
+        LOGGER.log(Level.INFO, "Consultando la Publicacion con  el id={0}", id);
+        return em.find(PublicacionEntity.class, id);
+    }
+
+    /**
+     * Metodo encargado de actualizar la Publicacion en la base de datos.
+     * @param pubEntity de la publicacion que se quiere actualizar en la base de datos.
+     * @return La publicacion actualizado.
+     */
+    public PublicacionEntity update(PublicacionEntity pubEntity) 
+    {
+        LOGGER.log(Level.INFO, "Actualizando la Publicacion con id={0}", pubEntity.getId());
+        return em.merge(pubEntity);
+    }
+
+    /**
+     * Metodo encargado de borrar la publicacion en la base de datos.
+     * @param pubEntity la publicacion que se quiere borrar en la base de datos.
+     */
+
+    public void delete(PublicacionEntity pubEntity) 
+    {
+        LOGGER.log(Level.INFO, "Borrando la Publicacion  con id={0}", pubEntity.getId());
+        em.remove(pubEntity);
+    }
+
 }
