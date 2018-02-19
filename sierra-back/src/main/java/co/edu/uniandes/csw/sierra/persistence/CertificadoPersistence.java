94e062b7860bf4b07fa264e71e7aa68649512536
rodrigo0097 <rj.gonzalez10@ISIS2603S1-0071.sis.virtual.uniandes.edu.co>
2018-02-18 20:49:13 -0500

    persistance

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/CertificadoPersistence.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/CertificadoPersistence.java
@@ -6,11 +6,81 @@
 package co.edu.uniandes.csw.sierra.persistence;
 
 import co.edu.uniandes.csw.sierra.ejb.*;
+import co.edu.uniandes.csw.sierra.entities.CertificadoEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
- * @author jc.sanchez12
+ * @author rj.gonzalez10
  */
 public class CertificadoPersistence {
     
+    private static final Logger LOGGER = Logger.getLogger(CertificadoPersistence.class.getName());
+    @PersistenceContext( unitName = "SierraPU" )
+    protected EntityManager em;
+    
+    /**
+     * Crea un nuevo certificado.
+     * @param entity objeto certificado que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public CertificadoEntity create(CertificadoEntity entity)
+    {
+        LOGGER.info("Creando un certificado");
+        em.persist(entity);
+        LOGGER.info("Creando el certicado");
+        return entity;
+    }
+    
+    /**
+     * Busca el certificado con id que se envia por parametro
+     * @param id correspondiente al certificado buscado.
+     * @return La entidad certificado.
+     */
+    public CertificadoEntity find (Long id)
+    {
+        LOGGER.log(Level.INFO, "Obteniendo certificado por el id", id);
+        return em.find(CertificadoEntity.class, id);
+    }
+    
+    /**
+     * Devuelve todos los certificados de la base de datos.
+     * @return una lista con todos los certificados.
+     */
+    public List<CertificadoEntity> findAll()
+    {
+         LOGGER.info("Consultando todos los certificados");
+         TypedQuery query = em.createQuery("select u from CertificadoEntity u", CertificadoEntity.class);
+         return query.getResultList();
+    }
+    
+    /**
+     * Actualiza la informacion de un cliente
+     * @param entity.  El certificado que va actualizar.
+     *@return el certificado actualizado.
+     */
+    public CertificadoEntity update(CertificadoEntity entity)
+    {
+       LOGGER.log(Level.INFO, "Actualizando certificado", CertificadoEntity.class);
+       return em.merge(entity);
+    }
+    
+    /**
+     * Elimina un cliente con el id que se envia por parametro
+     * @param  id: id correspondiente al certificado a eliminar
+     */
+    public void delete(Long id)
+    {
+        LOGGER.log(Level.INFO,"Eliminando certificado", CertificadoEntity.class);
+        CertificadoEntity entity = find(id);
+        
+        em.remove(entity);
+    }
+
+
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/RazaPersistence.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/persistence/RazaPersistence.java
@@ -5,10 +5,84 @@
  */
 package co.edu.uniandes.csw.sierra.persistence;
 
+import co.edu.uniandes.csw.sierra.entities.CertificadoEntity;
+import co.edu.uniandes.csw.sierra.entities.ClienteEntity;
+import co.edu.uniandes.csw.sierra.entities.RazaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 /**
  *
- * @author jc.sanchez12
+ * @author rj.gonzalez10
  */
 public class RazaPersistence {
+        private static final Logger LOGGER = Logger.getLogger(RazaPersistence.class.getName());
+    @PersistenceContext( unitName = "SierraPU" )
+    protected EntityManager em;
     
+    /**
+     * Crea un nuevo cliente.
+     * @param entity objeto raza que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public RazaEntity create(RazaEntity entity)
+    {
+        LOGGER.info("Creando una raza");
+        em.persist(entity);
+        LOGGER.info("Creando la raza");
+        return entity;
+    }
+    
+    /**
+     * Busca el cliente con id que se envia por parametro
+     * @param id correspondiente al la raza buscada.
+     * @return La entidad raza.
+     */
+    public RazaEntity find (Long id)
+    {
+        LOGGER.log(Level.INFO, "Obteniendo raza por el id", id);
+        return em.find(RazaEntity.class, id);
+    }
+    
+    /**
+     * Devuelve todas las razas de la base de datos.
+     * @return una lista con todas las razas.
+     */
+    public List<RazaEntity> findAll()
+    {
+         LOGGER.info("Consultando todas las razas");
+         TypedQuery query = em.createQuery("select u from RazaEntity u", RazaEntity.class);
+         return query.getResultList();
+    }
+    
+    /**
+     * Actualiza la informacion de una raza
+     * @param entity.  la raza que va actualizar.
+     *@return la raza actualizada.
+     */
+    public RazaEntity update(RazaEntity entity)
+    {
+       LOGGER.log(Level.INFO, "Actualizando raza", RazaEntity.class);
+       return em.merge(entity);
+    }
+    
+    /**
+     * Elimina una raza con el id que se envia por parametro
+     * @param  id: id correspondiente a la raza a eliminar
+     */
+    public void delete(Long id)
+    {
+        LOGGER.log(Level.INFO,"Eliminando raza", RazaEntity.class);
+       RazaEntity entity = find(id);
+        
+        em.remove(entity);
+    }
+
+
 }
+
+
