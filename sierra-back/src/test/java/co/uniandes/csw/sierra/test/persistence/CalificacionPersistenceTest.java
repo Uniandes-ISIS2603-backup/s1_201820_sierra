97e19282b982206001e998f01fc2e559a47579f3
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-19 09:17:52 -0500

    El XML modificado

---
+++ sierra-back/src/test/java/co/uniandes/csw/sierra/test/persistence/CalificacionPersistenceTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.uniandes.csw.sierra.test.persistence;
+
+import co.edu.uniandes.csw.sierra.entities.CalificacionEntity;
+import co.edu.uniandes.csw.sierra.persistence.CalificacionPersistence;
+/*
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+*/
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.junit.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+/**
+ *
+ * @author jd.zambrano
+ */
+@RunWith(Arquillian.class)
+public class CalificacionPersistenceTest {
+    
+    /**
+     * @return Devuelve el jar que se va a desplegar en glassfish 
+     */
+    @Deployment
+    public static JavaArchive createDeployment(){
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(CalificacionEntity.class.getPackage())
+                .addPackage(CalificacionPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Inject
+    private CalificacionPersistence calificacionPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    
+    /**
+     * Prueba para crear un Editorial.
+     *
+     *
+     */
+    @Test
+    public void createEditorialTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+        CalificacionEntity result = calificacionPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        CalificacionEntity entity = em.find(CalificacionEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+
+    
+    
+    
+}
