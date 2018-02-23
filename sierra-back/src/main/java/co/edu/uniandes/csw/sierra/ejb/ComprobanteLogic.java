121c39b9b593b357226c4720e5d7d8ff75e1014e
Jhonatan Amórtegui <ja.amortegui10@uniandes.edu.co>
2018-02-23 18:34:08 -0500

    Implementando ComprobanteLogic y FacturaLogic.

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/ComprobanteLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/ComprobanteLogic.java
@@ -5,10 +5,62 @@
  */
 package co.edu.uniandes.csw.sierra.ejb;
 
+import co.edu.uniandes.csw.sierra.entities.ComprobanteEntity;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sierra.persistence.ComprobantePersistence;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
  * @author jc.sanchez12
  */
+@Stateless
 public class ComprobanteLogic {
     
+    private static final Logger LOGGER = Logger.getLogger(ComprobanteLogic.class.getName());
+    
+    @Inject
+    private ComprobantePersistence persistence; 
+    
+    public ComprobanteEntity create(ComprobanteEntity entity)throws BusinessLogicException
+    {
+        LOGGER.info("Inicia proceso de cración de una entidad de Comprobante");
+        
+        if(persistence.find(entity.getId()) != null)
+            throw new BusinessLogicException("Ya existe una entidad Comprobante con e id dado: " + entity.getId());
+        
+        persistence.create(entity);
+        LOGGER.info("Termmína proceso de creación de la entidad de Comprobante.");
+        
+        return entity;
+        
+    }
+    
+    public List<ComprobanteEntity> getAll()
+    {
+        LOGGER.info("Inicia el proceso de consultar todas las entidades de Comprobante.");
+        List<ComprobanteEntity> entitys = persistence.findAll();
+        LOGGER.info("Termina el proceso de consultar todas las entidades de Comprobante.");
+        return entitys;
+    }
+    
+    public ComprobanteEntity getById(Long id)
+    {
+        return persistence.find(id);
+    }
+    
+    public ComprobanteEntity update(ComprobanteEntity entity)throws BusinessLogicException
+    {
+        return persistence.update(entity);
+    }
+    
+    public void delete(Long id)
+    {
+        LOGGER.info("Inicia el proceso de borrar una entidad de Comprobante.");
+        persistence.delete(id);
+        LOGGER.info("Termína el proceso de borrar una entidad de Comprobante.");
+    }
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/FacturaLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/FacturaLogic.java
@@ -5,10 +5,59 @@
  */
 package co.edu.uniandes.csw.sierra.ejb;
 
+import co.edu.uniandes.csw.sierra.entities.FacturaEntity;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sierra.persistence.FacturaPersistence;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
  * @author jc.sanchez12
  */
+@Stateless
 public class FacturaLogic {
     
+    private final static Logger LOGGER = Logger.getLogger(FacturaLogic.class.getName());
+    
+    @Inject
+    private FacturaPersistence persistence;
+    
+    public FacturaEntity create(FacturaEntity entity)throws BusinessLogicException
+    {
+        LOGGER.info("Iniciando el proceso de creación de una entidad de Factura.");
+        if(persistence.find(entity.getId()) != null)
+            throw new BusinessLogicException("Ya existe una entidad de Factura con el mismo id: " + entity.getId());
+        
+        persistence.create(entity);
+        LOGGER.info("Terminando el proceso de creación de una entidad de factura.");
+        return entity;
+    }
+    
+    public List<FacturaEntity> getAll()
+    {
+        LOGGER.info("Comienza el proceso de consultar todas las entidaddes de Factura existentes.");
+        List<FacturaEntity> entities = persistence.findAll();
+        LOGGER.info("Termína el proceso de consultar todas las entidades de Factura existentes.");
+        return entities;
+    }
+    
+    public FacturaEntity getById(Long id)
+    {
+        return persistence.find(id);
+    }
+    
+    public FacturaEntity update(FacturaEntity entity)
+    {
+        return persistence.update(entity);
+    }
+    
+    public void delete(Long id)
+    {
+        LOGGER.info("Inicia el proceso de eliminar una entidad de factura.");
+        persistence.delete(id);
+        LOGGER.info("Termína el proceso de eliminar una entidad de factura.");
+    }
 }
