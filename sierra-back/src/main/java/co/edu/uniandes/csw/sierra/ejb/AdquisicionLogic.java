32c758d4b2575376268ba9aca0612a53d100d6e4
Juan David Zambrano <jd.zambrano@uniandes.edu.co>
2018-02-25 23:35:55 -0500

    Se implementan Adquisicion y Calificacion Logic

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/AdquisicionLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/AdquisicionLogic.java
@@ -1,14 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    /*
+MIT License
+
+Copyright (c) 2017 ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 package co.edu.uniandes.csw.sierra.ejb;
 
+import co.edu.uniandes.csw.sierra.entities.AdquisicionEntity;
+import co.edu.uniandes.csw.sierra.persistence.AdquisicionPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
- * @author jc.sanchez12
+ * @author Juan David Zambrano
  */
+@Stateless
 public class AdquisicionLogic {
+    /**
+     * Logger, self explanatory
+     */
+    private static final Logger LOGGER = Logger.getLogger(AdquisicionLogic.class.getName());
+    
+    /**
+     * objeto de persistencia de Adquisicion
+     */
+    @Inject
+    private AdquisicionPersistence persistencia;
+    
+    /**
+     * Revisa que la entidad que se quiere crear cumpla las reglas de negocio y
+     * la crea
+     * @param ent la entidad que se quiere persistir
+     * @return la entidad persistida con el id autogenerado
+     */
+    public AdquisicionEntity create(AdquisicionEntity ent){
+        LOGGER.info("Creando una entidad de Adquisicion");
+        //TODO: Definir reglas de negocio
+        persistencia.create(ent);
+        LOGGER.info("Termina la creacion de la entidad de Adquisicion");
+        return ent;
+    }
+    
+    /**
+     * Obtiene todas las entidades de Adquisicion
+     * @return 
+     */
+    public List<AdquisicionEntity> getAll(){
+        LOGGER.info("Consultando todas las entidades de Adquisicion");
+        List<AdquisicionEntity> r = persistencia.findAll();
+        LOGGER.info("consultadas todas las entidades de Adquisicion");
+        return r;
+    }
+    /**
+     * Obtiene una entidad de ADquiicion con base en un Id dado 
+     * @param id el Id que se quiere encontrar
+     * @return la Adquisicion con el Id dado, Null si no lo encuentra
+     */
+    public AdquisicionEntity getById(Long id){
+        LOGGER.log(Level.INFO, "Buscando la Adquisicion con el id={0}", id);
+        return persistencia.find(id);
+    }
+    /**
+     * Actualiza una Adquisicion 
+     * @param ent la entidad con los datos que se quieren actualizar
+     * @return la entidad con los cambios ya realizados
+     */
+    public AdquisicionEntity update(AdquisicionEntity ent){
+        //TODO: Agregar reglas de negocio
+        LOGGER.log(Level.INFO, "Actualizando la entidad de Adquisicion con el id={0}", ent.getId());
+        return persistencia.update(ent);
+    }
+    
+    /**
+     * Elimina una Adquisicion
+     * @param ent la adquiisicion que se desea eliminar
+     */
+    public void delete(AdquisicionEntity ent){
+        LOGGER.log(Level.INFO, "Eliminando la Adquisicion con id ={0}", ent.getId());
+        persistencia.delete(ent);
+    }
+    
+    
+    
+    
     
 }
