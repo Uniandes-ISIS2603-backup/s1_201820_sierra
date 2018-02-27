b6063f63dc76fc718afab434652f876d9607ed16
Estebangp <de.gutierrez@uniandes.edu.co>
2018-02-26 19:17:36 -0500

    logica y pruebas de cliente y medio de pago

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/MedioDePagoLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/MedioDePagoLogic.java
@@ -4,11 +4,101 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.sierra.ejb;
-
+import co.edu.uniandes.csw.sierra.entities.MedioDePagoEntity;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sierra.persistence.MedioDePagoPersistence;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 /**
  *
  * @author jc.sanchez12
  */
+@Stateless
 public class MedioDePagoLogic {
+    
+    @Inject 
+    private MedioDePagoPersistence persistence;
+    
+    /**
+     * Crea una nueva entidad MedioDePago.
+     * @param entity La entidad de tipo MedioDePago que se va a persistir. 
+     * @return La entidad luego de persistirla.
+     * @throws BusinessLogicException Si ya existe un MedioDePago con esa informacion.
+     */
+    public MedioDePagoEntity createMedioDePago(MedioDePagoEntity entity) throws BusinessLogicException
+    {
+        LOGGER.info("Inicia el proceso de creacion de un nuevo medio de pago");
+        MedioDePagoEntity medio = persistence.find(entity.getId());
+        if (medio != null)
+        {
+            throw new BusinessLogicException("Ya existe un medio de pago con el nombre:" + entity.getName());
+        }
+        persistence.create(entity);
+        LOGGER.info("Termina el proceso de creacion del medio de pago");
+        return entity;
+    }
+    /***
+     * Obtiene todos los MedioDePago que hay en la base de datos.
+     * @return Lista de la entidades de tipo MedioDePago.
+     */
+    public List<MedioDePagoEntity> getMediosDePago()
+    {
+        LOGGER.info("Inicia el proceso de consultar todos los medio de pago");
+        List<MedioDePagoEntity> medios = persistence.findAll();
+        LOGGER.info("Termina el proceso de consultar todos los medio de pago");
+        return medios;
+    }
+    /**
+     * Obtiene un MedioDePago con el id especificado.
+     * @param id El identificador del MedioDePago buscado.
+     * @return El MedioDePago correspondiente a el id.
+     */
+    public MedioDePagoEntity getMedioDePago(Long id)
+    {
+        LOGGER.log(Level.INFO,"Inicia el proceso de consultar un medio de pago con id={0}", id);
+        MedioDePagoEntity medio = persistence.find(id);
+        if(medio == null)
+        {
+            LOGGER.log(Level.SEVERE, "El medio de pago con el id={0} no existe", id);
+        }
+        LOGGER.log(Level.SEVERE,"Termina el proceso de consultar un medio de pago con id={0}", id);
+        return medio;
+    }
+    /**
+     * Actualiza un MedioDePago con el id dado.
+     * @param id Identificador del MedioDePago que se quiere actualizar.
+     * @param entity Nueva informacion del MedioDePago.
+     * @return La entidad MedioDePago con la nueva informacion.
+     */
+    public MedioDePagoEntity updateMedioDePago(Long id, MedioDePagoEntity entity)
+    {
+        LOGGER.log(Level.SEVERE, "Inicia el proceso de actualizar un medio de pago");
+        if(getMedioDePago(id) == null)
+        {
+            LOGGER.log(Level.SEVERE, "El medio de pago con el id={0} no existe para ser actualizado", entity.getId());
+        }
+        persistence.update(entity);
+        LOGGER.log(Level.SEVERE, "Termina el proceso de actualizar un medio de pago");
+        return entity;
+    }
+    /***
+     * Elimina un MedioDePago segun el id.
+     * @param id Identificador del MedioDePago a eliminar.
+     */
+    public void deleteMedioDePago(Long id)
+    {
+         LOGGER.log(Level.SEVERE, "Inicia el proceso de eliminar un cliente");
+        if(getMedioDePago(id)==null)
+        {
+            LOGGER.log(Level.SEVERE, "El medio de pago con el id={0} no existe para ser eliminado", id);
+        }
+        persistence.delete(id);
+        LOGGER.log(Level.SEVERE, "Termina el proceso de eliminar un medui de pago");
+    }
+    
     
 }
