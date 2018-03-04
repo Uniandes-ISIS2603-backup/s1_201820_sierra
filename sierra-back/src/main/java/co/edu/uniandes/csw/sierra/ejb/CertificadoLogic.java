d73b6d8844a5b254ba0daa6ee0030ee41c6bb872
rj.gonzalez10 <rj.gonzalez10@ISIS2603S1-0071.sis.virtual.uniandes.edu.co>
2018-03-04 12:47:45 -0500

    Logic terminados

--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/CertificadoLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/CertificadoLogic.java
@@ -5,10 +5,57 @@
  */
 package co.edu.uniandes.csw.sierra.ejb;
 
+import co.edu.uniandes.csw.sierra.entities.CertificadoEntity;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sierra.persistence.CertificadoPersistence;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
 /**
  *
- * @author jc.sanchez12
+ * @author rj.gonzalez10
  */
 public class CertificadoLogic {
+       private final static Logger LOGGER = Logger.getLogger(CertificadoLogic.class.getName());
+    
+    @Inject
+    private CertificadoPersistence persistence;
+    
+    public CertificadoEntity create(CertificadoEntity entity)throws BusinessLogicException
+    {
+        LOGGER.info("Iniciando el proceso de creación de una entidad de certificado.");
+        if(persistence.find(entity.getId()) != null)
+            throw new BusinessLogicException("Ya existe una entidad de certificado con el mismo id: " + entity.getId());
+        
+        persistence.create(entity);
+        LOGGER.info("Terminando el proceso de creación de una entidad de certificado.");
+        return entity;
+    }
+    
+    public List<CertificadoEntity> getAll()
+    {
+        LOGGER.info("Comienza el proceso de consultar todas las entidaddes de certificado existentes.");
+        List<CertificadoEntity> entities = persistence.findAll();
+        LOGGER.info("Termína el proceso de consultar todas las entidades de certificado existentes.");
+        return entities;
+    }
+    
+    public CertificadoEntity getById(Long id)
+    {
+        return persistence.find(id);
+    }
+    
+    public CertificadoEntity update(CertificadoEntity entity)
+    {
+        return persistence.update(entity);
+    }
+    
+    public void delete(Long id)
+    {
+        LOGGER.info("Inicia el proceso de eliminar una entidad de certificado.");
+        persistence.delete(id);
+        LOGGER.info("Termína el proceso de eliminar una entidad de certificado.");
+    }
     
 }
--- sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/RazaLogic.java
+++ sierra-back/src/main/java/co/edu/uniandes/csw/sierra/ejb/RazaLogic.java
@@ -5,10 +5,104 @@
  */
 package co.edu.uniandes.csw.sierra.ejb;
 
+import co.edu.uniandes.csw.sierra.entities.EspecieEntity;
+import co.edu.uniandes.csw.sierra.entities.RazaEntity;
+import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.sierra.persistence.RazaPersistence;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
- * @author jc.sanchez12
+ * @author rj.gonzalez10
  */
-public class RazaLogic {
+@Stateless
+public class RazaLogic
+{
+    private static final Logger LOGGER = Logger.getLogger( SierraLogic.class.getName( ) );
+
+	@Inject
+	private RazaPersistence persistence;
     
+        /**
+         * Crea una  entity en la  base de datos  de tipo Raza
+         * @param entity Bojeto con los datos  de la nueva  RazaEntity 
+         * @return Objeto de  tipo RazaEntity con los  datos nuevos  
+         * @throws BusinessLogicException 
+         */
+     public RazaEntity create( RazaEntity entity) throws BusinessLogicException
+     {
+         LOGGER.info( "Inicia proceso de creación de una entidad de Raza" );
+         if (persistence.findByName(entity.getNombreRaza())!=null)
+         {
+             throw new BusinessLogicException( "Ya existe una entidad de Raza con el nombre \"" + entity.getName( ) + "\"" );
+         }
+         else{
+             persistence.create(entity);
+             LOGGER.info( "Termina proceso de creación de una entidad de Raza" );
+             return entity;
+         }
+     }
+     
+     
+    /**
+     * Obtiene la lista de los registros de Raza.
+     *
+     * @return Colección de objetos de RazaEntity.
+     */
+     public List<RazaEntity> getAll()
+     {
+                LOGGER.info( "Inicia proceso de consultar todas las entidades de Raza" );
+		List<RazaEntity> entities = persistence.findAll( );
+                LOGGER.info( "Termina proceso de consultar todas las entidades de Raza" );
+		return entities;
+     }
+     
+     
+     /**
+     * Obtiene los datos de una instancia de Raza a partir de su ID.
+     *
+     * @param id Identificador de la instancia a consultar
+     * @return Instancia de EsoeciEntity con los datos de la raza consultada.
+     */
+     public RazaEntity getById( Long id )
+     {
+	return persistence.find(id );
+     }
+     
+     /**
+     * Obtiene los datos de una instancia de Raza a partir de su nombre.
+     *
+     * @param nombre nombre  de la instancia a consultar
+     * @return Instancia de RazaEntity con los datos de la especie consultada.
+     */
+     public RazaEntity getByName( String nombre )
+     {
+	return persistence.findByName(nombre );
+     }
+     
+     /**
+     * Actualiza la información de una instancia de Raza.
+     *
+     * @param entity Instancia de RazaEntity con los nuevos datos.
+     * @return Instancia de RazaEntity con los datos actualizados.
+     */
+    public RazaEntity update(RazaEntity entity)
+    {
+        
+        return persistence.update(entity);
+    }
+    
+    /**
+     * Elimina una instancia de Raza de la base de datos.
+     *
+     * @param id Identificador de la instancia a eliminar.
+     */
+    public void delete(Long id) {
+        
+        persistence.delete(id);
+    }
+
 }
