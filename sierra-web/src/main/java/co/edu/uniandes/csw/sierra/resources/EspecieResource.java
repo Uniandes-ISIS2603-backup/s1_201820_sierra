c136bdd69b833fa50d3223827815c205f2c5bc5f
Juan Camilo Sanchez Pardo <jc.sanchez12@sis.virtual.uniandes.edu.co>
2018-03-04 23:28:29 -0500

    Resource de especie completo

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/EspecieResource.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/EspecieResource.java
@@ -5,13 +5,15 @@
  */
 package co.edu.uniandes.csw.sierra.resources;
 
-import co.edu.uniandes.csw.sierra.dtos.CalificacionDetailDTO;
-import co.edu.uniandes.csw.sierra.dtos.EspecieDTO;
+
 import co.edu.uniandes.csw.sierra.dtos.EspecieDetailDTO;
+import co.edu.uniandes.csw.sierra.ejb.EspecieLogic;
+import co.edu.uniandes.csw.sierra.entities.EspecieEntity;
 import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -44,35 +46,153 @@ import javax.ws.rs.Produces;
 @Consumes( "application/json" )
 @RequestScoped
 public class EspecieResource {
+    
+    /**
+     * Injecccion de la logica
+     */
+    @Inject
+    private EspecieLogic especieLogic;
    
-    @POST
-    public EspecieDetailDTO createEspecie(EspecieDetailDTO dto){
-        return dto;
+ 
+    /**
+     * Convierte una lista de entities a una lista de Detaildto.
+     *
+     * @param entityList Lista  a convertir.
+     * @return Lista convertida.
+     * 
+     */
+    private List<EspecieDetailDTO> listEntity2DTO(List<EspecieEntity> entityList) {
+        List<EspecieDetailDTO> list = new ArrayList<>();
+        for (EspecieEntity entity : entityList) {
+            list.add(new EspecieDetailDTO(entity));
+        }
+        return list;
     }
     
+    /**
+     * <h1>GET /api/especies : Obtener todos las especies.</h1>
+     *
+     * <pre>Busca y devuelve todos las especies que existen en la aplicacion.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve todos las especies de la aplicacion.</code> 
+     * </pre>
+     * @return JSONArray {@link EspecieDetailDTO} - Las especies encontrados en la aplicación. Si no hay ninguno retorna una lista vacía.
+     */  
     @GET
     public List<EspecieDetailDTO> getEspecies(){
-        return new ArrayList<>();
+       return listEntity2DTO(especieLogic.getAll());
     }
     
+     /**
+     * <h1>GET /api/especies/{id} : Obtener especie por id.</h1>
+     *
+     * <pre>Busca la especie con el id asociado recibido en la URL y lo devuelve.
+     *
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve la especie correspondiente al id.
+     * </code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found No existe una especie  con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la especie  que se esta buscando. Este debe ser una cadena de dígitos.
+     * @return JSON {@link EspecieDetailDTO} - El autor buscado
+     * @throws BusinessLogicException {@link BusinessLogicException} - Error de lógica que se genera cuando no se encuentra el autor.
+     */
     @GET
     @Path( "{id: \\d+}" )
-    public EspecieDetailDTO getEspecie( @PathParam( "id" ) Long id )
+    public EspecieDetailDTO getEspecie( @PathParam( "id" ) Long id ) throws BusinessLogicException
     {
-        return null;
+        EspecieEntity especie= especieLogic.getById(id);
+        if (especie==null) {
+             throw new BusinessLogicException("La compania no existe.");
+        }
+        return new EspecieDetailDTO(especie);
     }
     
+     /**
+     * <h1>POST /api/especies : Crear un autor.</h1>
+     *
+     * <pre>Cuerpo de petición: JSON {@link EspecieDetailDTO}.
+     * 
+     * Crea una nueva especie con la informacion que se recibe en el cuerpo 
+     * de la petición y se regresa un objeto identico con un id auto-generado 
+     * por la base de datos.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Creo la especie  .
+     * </code>
+     * </pre>
+     * @param dto {@link EspecieDetailDTO} -La especie  que se desea guardar.
+     * @return JSON {@link EspecieDetailDTO}  - La especie se a guardado con el atributo id autogenerado.
+     * @throws co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException
+     */
+    @POST
+    public EspecieDetailDTO createEspecie(EspecieDetailDTO dto) throws BusinessLogicException{
+        return new EspecieDetailDTO(especieLogic.create(dto.toEntity()));
+    }
+    
+    /**
+     * <h1>PUT /api/especies/{id} : Actualizar especie con el id dado.</h1>
+     * <pre>Cuerpo de petición: JSON {@link EspecieDetailDTO}.
+     *
+     * Actualiza la especie  con el id recibido en la URL con la información que se recibe en el cuerpo de la petición.
+     *
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Actualiza la especie con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una especie con el id dado.
+     * </code> 
+     * </pre>
+     * @param id Identificador de la especie que se desea actualizar. Este debe ser una cadena de dígitos.
+     * @param dDTO {@link EspecieDetailDTO} La especie que se desea guardar.
+     * @return JSON {@link EspecieDetailDTO} - La especie guardado.
+     * @throws BusinessLogicException {@link BusinessLogicException} - Error de lógica 
+     */
     @PUT
     @Path( "{id: \\d+}" )
     public EspecieDetailDTO updateEspecie( @PathParam( "id" ) Long id, EspecieDetailDTO dDTO ) throws BusinessLogicException
     {
-	return dDTO;
+        EspecieEntity entity = dDTO.toEntity();
+        entity.setId(id);
+        EspecieEntity oldEntity = especieLogic.getById(id);
+        if (oldEntity == null) {
+            throw new BusinessLogicException("La especie no existe");
+        }
+        entity.setMascotaS(oldEntity.getMascotas());
+        entity.setRazas(oldEntity.getRazas());
+        return new EspecieDetailDTO(especieLogic.update(entity));
     }
     
+    /**
+     * <h1>DELETE /api/especies/{id} : Borrar especie por id.</h1>
+     *
+     * <pre>Borra la especie  con el id asociado recibido en la URL.
+     *
+     * Códigos de respuesta:<br>
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Elimina la especie  correspondiente al id dado.</code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 404 Not Found. No existe una especie  con el id dado.
+     * </code>
+     * </pre>
+     * @param id Identificador de la especie que se desea borrar. Este debe ser una cadena de dígitos.
+     * @throws co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException
+     */
     @DELETE
     @Path( "{id: \\d+}" )
-    public void deleteEspecie( @PathParam( "id" ) Long id )
+    public void deleteEspecie( @PathParam( "id" ) Long id ) throws BusinessLogicException
     {
+        EspecieEntity entity = especieLogic.getById(id);
+        if (entity == null) {
+            throw new BusinessLogicException("La especie no existe");
+        }
+        especieLogic.delete(id);
     	
     }
 }
