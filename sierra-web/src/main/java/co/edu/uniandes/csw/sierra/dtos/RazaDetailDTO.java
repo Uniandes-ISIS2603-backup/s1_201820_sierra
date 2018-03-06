56631818af924c8f845ae71a0bbf5c29aae55d0a
rj.gonzalez10 <rj.gonzalez10@ISIS2603S1-0071.sis.virtual.uniandes.edu.co>
2018-03-05 23:13:25 -0500

    Cambio en datail dto y Resource

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/RazaDetailDTO.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/dtos/RazaDetailDTO.java
@@ -5,6 +5,12 @@
  */
 package co.edu.uniandes.csw.sierra.dtos;
 
+import co.edu.uniandes.csw.sierra.entities.MascotaAdoptadaEntity;
+import co.edu.uniandes.csw.sierra.entities.MascotaEntity;
+import co.edu.uniandes.csw.sierra.entities.RazaEntity;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rj.gonzalez10
@@ -14,5 +20,74 @@ public class RazaDetailDTO extends RazaDTO {
     {
         super();
     }
+    
+    /**
+     * Lista de mascotas que contiene una  Raza
+     */
+    private List<MascotaDTO> mascotas;
+    
+     /**
+     * Crea un objeto RazaDetailDTO a partir de un objeto RazaEntity
+     * incluyendo los atributos de RazaDTO.
+     *
+     * @param entity Entidad Raza
+     * Entity desde la cual se va a crear el nuevo
+     * objeto.
+     *
+     */
+    public RazaDetailDTO(RazaEntity entity)
+    {
+        super(entity);
+        if (entity!=null) {
+            
+            mascotas= new ArrayList<>();
+            for (MascotaEntity entityMascotas : entity.getMascotas()) {
+                mascotas.add(new MascotaDTO(entityMascotas));
+            }
+        }
+        
+    }
+    /**
+     * Convierte un objeto RazaDetailDTO a RazaEntity incluyendo los
+     * atributos de RazaDTO.
+     *
+     * @return Nueva objeto RazaEntity.
+     *
+     */
+    @Override
+    public RazaEntity toEntity() {
+        RazaEntity entity = super.toEntity();
+        if (mascotas!=null) {
+             List<MascotaEntity> mascotasEntity=new ArrayList<>();
+             for (MascotaDTO mascotaDto : mascotas) {
+             if (MascotaAdopcionDTO.class.isInstance(mascotaDto))
+             {
+                 mascotasEntity.add(mascotaDto.toEntity(new MascotaAdoptadaEntity()));
+                 entity.setMascotas(mascotasEntity);
+             }
+             else{
+                 mascotasEntity.add(mascotaDto.toEntity(new MascotaAdoptadaEntity()));
+                 entity.setMascotas(mascotasEntity);
+             }
+            }
+             entity.setMascotas(mascotasEntity);
+        }
+        
+        return entity;
+    }
+    /**
+     * @return the mascotas
+     */
+    public List<MascotaDTO> getMascotas() {
+        return mascotas;
+    }
+    /**
+     * @param mascotas the mascotas to set
+     */
+    public void setMascotas(List<MascotaDTO> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    
     
 }
