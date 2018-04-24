ae4f0720b7e13adbbfafdc70229986416354f757
ja.penat <ja.penat@ISIS2603S1-0084.sis.virtual.uniandes.edu.co>
2018-04-24 00:31:13 -0500

    Correccion en el despliegue de acontecimiento y publicacion

---
+++ sierra-web/src/main/webapp/src/modules/publicacion/publicacion.ctrl.js
@@ -0,0 +1,18 @@
+(function(ng){
+    
+    var modulo = ng.module("publicacionModule");
+    modulo.constant("publicacionContext", "api/publicaciones");
+    modulo.controller('publicacionCtrl', ['$scope', '$http', 'publicacionContext',
+                      function($scope, $http, publicacionContext)
+                      {
+                       $scope.publicaciones = {};
+                       $http.get('data/publicaciones.json').then(function (response) 
+                                                                   {
+                                                                    $scope.publicaciones = response.data;   
+                                                                   }
+                                                                   );
+} 
+]);
+})
+
+
