f2fc7ac3fbc1bdd558ef942ba82c6806641534ed
Jhonatan Amórtegui <ja.amortegui10@uniandes.edu.co>
2018-05-03 02:24:44 -0500

    Cambios

--- sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/FacturaResource.java
+++ sierra-web/src/main/java/co/edu/uniandes/csw/sierra/resources/FacturaResource.java
@@ -11,7 +11,6 @@ import co.edu.uniandes.csw.sierra.ejb.AdquisicionLogic;
 import co.edu.uniandes.csw.sierra.ejb.ComprobanteLogic;
 import co.edu.uniandes.csw.sierra.ejb.FacturaLogic;
 import co.edu.uniandes.csw.sierra.entities.AdquisicionEntity;
-import co.edu.uniandes.csw.sierra.entities.ComprobanteEntity;
 import co.edu.uniandes.csw.sierra.entities.FacturaEntity;
 import co.edu.uniandes.csw.sierra.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.sierra.mappers.BusinessLogicExceptionMapper;
@@ -27,7 +26,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 
 /**
@@ -46,8 +44,8 @@ import javax.ws.rs.WebApplicationException;
  * @author ja.amortegui10
  */
 //TODO: Revisar el path para llegar a este recurso
-@Path( "adquisiciones/{adquisicionId: \\d}/facturas" )
-@Produces( "application/json" )
+@Path( "/facturas" )
+@Produces( "application/json")
 @Consumes( "application/json" )
 @RequestScoped
 public class FacturaResource {
@@ -70,40 +68,7 @@ public class FacturaResource {
     @Inject
     private ComprobanteLogic logicComprobante;
     
-    /**
-     * <h1> POST /api/factura : Crea una factura. </h1>
-     * <pre>
-     * Cuerpo de la peticion: JSON (@link FacturaDetailDTO).
-     * Crea una nueva factura con la informacion que entra por el cuerpo de la petición.
-     * Retorna un nuevo objeto identico con un id autogenerado por la base de datos.
-     * Códigos de respuesta:
-     * <code style="color:mediumseagreen; backround-color: #eaffe0;">
-     * 200 OK La factura fue creada.
-     * </code>
-     * <code style="color: #c7254e; background-color: #f9f2f4;">
-     * 412 Precodition Failed: Ya existe una factura.
-     * </code>
-     *  </pre>
-     * @param dto {@link FacturaDetailDTO} - La factura que se desea guardar
-     * @return JSON {@link FacturaDetailDTO} - La factura guardada con el atributo id generado
-     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de logica que se genera cuando ya existe una factura.
-     *
-     */
-    @POST
-    public FacturaDetailDTO createFactura(FacturaDetailDTO dto) throws BusinessLogicException
-    {
-        return new FacturaDetailDTO(logicFactura.create(dto.toEntity()));
-    }
     
-    private List<FacturaDetailDTO> listEntity2DTO(List<FacturaEntity> entities)
-    {
-        List<FacturaDetailDTO> dtos = new ArrayList<>();
-        for(FacturaEntity entityActual : entities)
-            dtos.add(new FacturaDetailDTO(entityActual));
-        
-        
-        return dtos;
-    }
     
     /**
      * <h1>GET /api/facturas : Obtener todas las facturas.</h1>
@@ -118,19 +83,22 @@ public class FacturaResource {
      */
     
     @GET
-    public FacturaDetailDTO getFacturas(@PathParam("adquisicionId") Long adquisicionId)throws WebApplicationException
+    public List<FacturaDetailDTO> getFacturas()throws WebApplicationException
     {
-        System.out.println("-->> El id: " + adquisicionId);
-        AdquisicionEntity adquisicion = logicAdquisicion.getById(adquisicionId);
-        if(adquisicion == null)
-            throw new WebApplicationException("La adquisición con el id dado no existe.");
-        
-        
-        return new FacturaDetailDTO(adquisicion.getFactura());
-        
+        return listEntity2DTO(logicFactura.getAll());
     }
     
-    /**
+     private List<FacturaDetailDTO> listEntity2DTO(List<FacturaEntity> entities)
+    {
+        List<FacturaDetailDTO> dtos = new ArrayList<>();
+        for(FacturaEntity entityActual : entities)
+            dtos.add(new FacturaDetailDTO(entityActual));
+        
+        
+        return dtos;
+    }
+    
+      /**
      * <h1>GET /api/facturas/{id} : Obtener una factura por id.</h1>
      * 
      * <pre>Busca la factura con el id asociado recibido en la URL y la devuelve.
@@ -158,6 +126,37 @@ public class FacturaResource {
         
         return new FacturaDetailDTO(encontrada);
     }
+     
+    /**
+     * <h1> POST /api/factura : Crea una factura. </h1>
+     * <pre>
+     * Cuerpo de la peticion: JSON (@link FacturaDetailDTO).
+     * Crea una nueva factura con la informacion que entra por el cuerpo de la petición.
+     * Retorna un nuevo objeto identico con un id autogenerado por la base de datos.
+     * Códigos de respuesta:
+     * <code style="color:mediumseagreen; backround-color: #eaffe0;">
+     * 200 OK La factura fue creada.
+     * </code>
+     * <code style="color: #c7254e; background-color: #f9f2f4;">
+     * 412 Precodition Failed: Ya existe una factura.
+     * </code>
+     *  </pre>
+     * @param dto {@link FacturaDetailDTO} - La factura que se desea guardar
+     * @return JSON {@link FacturaDetailDTO} - La factura guardada con el atributo id generado
+     * @throws BusinessLogicException {@link BusinessLogicExceptionMapper} - Error de logica que se genera cuando ya existe una factura.
+     *
+     */
+    @POST
+    public FacturaDetailDTO createFactura(FacturaDetailDTO dto) throws BusinessLogicException
+    {
+        return new FacturaDetailDTO(logicFactura.create(dto.toEntity()));
+    }
+    
+   
+    
+    
+    
+   
     
     /**
      * <h1>  PUT /api/facturas/{id} : Actualizar una entidad de factura con el id dado. </h1>
