d7e048f4cf87ce315232117a3c72a557553a292d
Estebangp <de.gutierrez@uniandes.edu.co>
2018-05-04 17:13:30 -0500

    correccion metodo en medioDePagoLogic

--- sierra-web/src/main/webapp/src/modules/mediosDePago/mediosDePago.delete.ctrl.js
+++ sierra-web/src/main/webapp/src/modules/mediosDePago/mediosDePago.delete.ctrl.js
@@ -3,7 +3,6 @@
     mod.constant("mediosDePagoContext", "api/mediosDePago");
     mod.controller('deleteMediosDePagoCtrl', ['$scope','$http','mediosDePagoContext','$state',
          function ($scope, $http, mediosDePagoContext, $state){
-             var mediosDePagoId = $state.params.mediosDePagoId;
              $scope.deleteMediosDePago = function(){
                  $http.delete(mediosDePagoContext+'/'+$state.params.mediosDePagoId).then(function(response){
                       $state.go('mediosDePagoList', {mediosDePagoId: response.data.id}, {reload: true});
