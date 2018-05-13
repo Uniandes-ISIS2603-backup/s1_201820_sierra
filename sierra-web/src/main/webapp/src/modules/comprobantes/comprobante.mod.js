9bb9c1c8018d7ea5d65a2fdda677b86bdda56cde
Jhonatan Amórtegui <ja.amortegui10@uniandes.edu.co>
2018-05-13 01:47:40 -0500

    Implementado factura edit, y ya casi factura delete. Implementados módulos comprobante, funcionan get y casi post.

--- sierra-web/src/main/webapp/src/modules/comprobantes/comprobante.mod.js
+++ sierra-web/src/main/webapp/src/modules/comprobantes/comprobante.mod.js
@@ -8,12 +8,60 @@
         $urlRouterProvider.otherwise("/comprobantesList");
         $stateProvider.state('comprobantesList',
         {
+            url:'/comprobantes/list',
+            views:{
+                mainView:{
+                    
+                    templateUrl: basePath+'list/comprobante.list.html',
+                    controller:'comprobanteCtrl',
+                    controllerAs: 'Ctrl'
+                }
+            }
+        })
+        .state('comprobantes',
+        {
             url:'/comprobantes',
             views:{
                 mainView:{
                     
-                    templateUrl: basePath+'comprobante.list.html',
+                    templateUrl: basePath+'comprobante.html',
                     controller:'comprobanteCtrl',
+                    controllerAs: 'Ctrl'
+                }
+            }
+        })
+        .state('comprobanteCreate',
+        {
+            url:'/comprobantes/create)',
+            views:{
+                mainView:{
+                    
+                    templateUrl: basePath+'create/comprobante.create.html',
+                    controller:'comprobanteCreateCtrl',
+                    controllerAs: 'Ctrl'
+                }
+            }
+        })
+        .state('comprobanteUpdate',
+        {
+            url:'/comprobantes/update',
+            views:{
+                mainView:{
+                    
+                    templateUrl: basePath+'update/comprobante.update.html',
+                    controller:'comprobanteUpdateCtrl',
+                    controllerAs: 'Ctrl'
+                }
+            }
+        })
+        .state('comprobanteDelete',
+        {
+            url:'/comprobantes/list',
+            views:{
+                mainView:{
+                    
+                    templateUrl: basePath+'comprobante.list.html',
+                    controller:'comprobanteDeleteCtrl',
                     controllerAs: 'Ctrl'
                 }
             }
--- sierra-web/src/main/webapp/src/modules/facturas/delete/factura.delete.ctrl.js
+++ sierra-web/src/main/webapp/src/modules/facturas/delete/factura.delete.ctrl.js
@@ -1,7 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function (ng)
+{
+    var mod = ng.module("facturaModule");
+    mod.constant("facturaContext", "api/facturas");
+    mod.controller('facturaDeleteCtrl', ['$scope', '$http', 'facturaContext', '$state', '$rootScope',
+        function ($scope, $http, facturaContext, $state, $rootScope) {
+            $rootScope.edit = false;
+            $scope.data = {};
+            var facturaId = $state.params.facturaId;
+            $scope.deleteFactura = function ()
+            {
+                $http.delete(facturaContext + "/" + facturaId).then(function (response) {
+                    $state.go('facturasList', {reload: true});
+                });
+            };
+        }
+
+    ]);
+}
+)(window.angular);
 
 
