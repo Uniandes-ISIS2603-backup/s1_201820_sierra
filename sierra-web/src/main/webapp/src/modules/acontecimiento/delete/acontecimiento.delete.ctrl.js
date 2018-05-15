2e0746951c26409ef358da24fec4e2391df3702b
ja.penat <ja.penat@ISIS2603S1-0084.sis.virtual.uniandes.edu.co>
2018-05-15 18:56:26 -0500

    Modulo (Acontecimiento) completamente terminado y corregido

--- sierra-web/src/main/webapp/src/modules/acontecimiento/delete/acontecimiento.delete.ctrl.js
+++ sierra-web/src/main/webapp/src/modules/acontecimiento/delete/acontecimiento.delete.ctrl.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+(function (ng){
+    var mod = ng.module('acontecimientoModule');
+    mod.constant("acontecimientoContext", "api/acontecimientos");
+    mod.controller('acontecimientoDeleteCtrl', ['$scope','$http','acontecimientoContext','$state',
+         function ($scope, $http, acontecimientoContext, $state){
+             $scope.deleteAcontecimiento = function(){
+                 $http.delete(acontecimientoContext+'/'+$state.params.acontecimientoId).then(function(response){
+                      $state.go('acontecimientoList', {acontecimientoId: response.data.id}, {reload: true});
+                 });
+             };
+         }
+    ]);
+    
+}
+)(window.angular);
