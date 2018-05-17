e2b75474e3af8e8b1e9e2214d56de3c707b87296
Juan Camilo Sanchez Pardo <jc.sanchez12@sis.virtual.uniandes.edu.co>
2018-05-16 23:12:22 -0500

    informacion para especies

--- sierra-web/src/main/webapp/src/modules/especies/update/especieedit.ctrl.js
+++ sierra-web/src/main/webapp/src/modules/especies/update/especieedit.ctrl.js
@@ -1,16 +1,16 @@
 (function (ng) {
     var mod = ng.module("especieModule");
     mod.constant("especiesContext", "api/especies");
-    mod.controller('especieeditCtrl', ['$scope', '$http', 'especiesContext', '$state', '$rootScope', '$filter',
+     mod.constant('razaContext','api/razas');
+    mod.controller('especieeditCtrl', ['$scope', '$http', 'especiesContext', '$state', '$rootScope', '$filter','razaContext',
 
-      function ($scope, $http, especiesContext, $state, $rootScope, $filter) {
+      function ($scope, $http, especiesContext, $state, $rootScope, $filter,razaContext) {
             $rootScope.edit = true;
-            
-             $http.get(especiesContext).then(function (response) { 
-                    $scope.especieRecords = response.data;
-                    $scope.currentEspecie = $filter('filter')($scope.especieRecords, {id: $state.params.especieId}, true)[0];
-                 
-                });
+            $scope.data = {};
+
+            $scope.selectedItems = [];
+
+            $scope.availableItems = [];
 
 
             var idEspecie = $state.params.especieId;
@@ -19,24 +19,62 @@
             //Consulto el autor a editar.
             $http.get(especiesContext + '/' + idEspecie).then(function (response) {
                 var especie = response.data;
-             
                     $scope.nombreEspecie = especie.nombre;
-                   
+                    $scope.id=especie.id;
                     $scope.ClasificacionEspecie = especie.clasificacion; 
                 
                
                     $scope.CaracteristicasEspecie = especie.caracteristicas;
           
                      $scope.ImagenEspecie = especie.imagen;
+                     $scope.razas=especie.razas;
+                     
+                     $scope.getRazas(especie.razas);
+                     $scope.getMascotas(especie.mascotas);
                 
             });
      
+            
+              $scope.getRazas = function (razas) {
+
+                $http.get(razaContext).then(function (response) {
+
+                    $scope.allRazas = response.data;
+                    $scope.razasEspecie = razas;
+
+                    var filteredRazas = $scope.allRazas.filter(function (raza) {
+                        return $scope.razasEspecie.filter(function (razaEspecie) {
+                            return razaEspecie.id === raza.id;
+                        }).length === 0;
+                    });
+
+                    var unFilteredRazas = $scope.allRazas.filter(function (raza) {
+                        return $scope.razasEspecie.filter(function (razaEspecie) {
+                            return razaEspecie.id === raza.id;
+                        }).length !== 0;
+                    });
+
+                    if ($scope.razasEspecie.length === 0) {
+
+                        $scope.availableItems = $scope.allRazas;
+
+                    } else {
+
+                        $scope.selectedItems = unFilteredRazas;
+                        $scope.availableItems = filteredRazas;
+                    }
+                });
+            };
+            
+            
+            
               $scope.createEspecie = function () {   
                 $http.put(especiesContext + "/" + idEspecie, {
                     nombre: $scope.nombreEspecie,
                     clasificacion: $scope.ClasificacionEspecie,
                     caracteristicas: $scope.CaracteristicasEspecie,
                     imagen: $scope.ImagenEspecie
+                    
                 }).then(function (response) {
                     $state.go('especiessList', {idEspecie: response.data.id}, {reload: true});
                 });
