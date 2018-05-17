a548580952284ff88da23f45aa316322dc1d2130
Juan Camilo Sanchez Pardo <jc.sanchez12@sis.virtual.uniandes.edu.co>
2018-05-17 04:23:17 -0500

    nada

--- sierra-web/src/main/webapp/src/modules/raza/new/razaCreate.ctrl.js
+++ sierra-web/src/main/webapp/src/modules/raza/new/razaCreate.ctrl.js
@@ -1,12 +1,21 @@
 (function (ng) {
     var mod = ng.module("razaModule");
-    mod.constant("razasContext", "api/razas");
-    mod.controller('razacreateCtrl', ['$scope', '$http', 'razasContext', '$state', '$rootScope',
+    mod.constant("razasContext", "api/razas",'especiesContext','api/especies');
+    mod.controller('razacreateCtrl', ['$scope', '$http', 'razasContext', '$state', '$rootScope','especiesContext',
                
-        function ($scope, $http, razasContext, $state, $rootScope) {
+        function ($scope, $http, razasContext, $state, $rootScope,especiesContext) {
             $rootScope.edit = false;
+            
+             $http.get(especiesContext).then(function (response) {
+                $scope.especiesRecords = response.data;
+            });
 
             $scope.createRaza = function () {
+               
+                $http.get(especiesContext).then(function (response) { 
+                    $scope.especiesRecords = response.data;
+                    $scope.currentEspecie = $filter('filter')($scope.especiesRecords, {id: $scope.data.especieId}, true)[0];
+                });
                 $http.post(razasContext,$scope.data).then(function (response) {
                     $state.go('razasList', {razaId: response.data.id}, {reload: true});
                 });
